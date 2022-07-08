5810010f569616971054a2a39bf743e834b9b8fd
NEET\Admin <neetkite@gmail.com>
2022-07-08 14:04:58 +0700

    Galaxy tutorial

--- pages/3d/particle.jsx
+++ pages/3d/particle.jsx
@@ -1,13 +1,17 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import styles from "./text.module.css";
-import { OrbitControls, Center } from "@react-three/drei";
+import { OrbitControls, Center, useTexture } from "@react-three/drei";
 import { useLayoutEffect, useRef } from "react";
 import * as THREE from "three";
 
 const ParticlePage = () => {
   return (
     <>
-      <Canvas camera={{ fov: 75 }} className={styles.haunted_canvas}>
+      <Canvas
+        dpr={[1, 2]}
+        camera={{ fov: 75 }}
+        className={styles.haunted_canvas}
+      >
         <OrbitControls />
         <Scence />
       </Canvas>
@@ -19,7 +23,7 @@ const Scence = () => {
   return (
     <>
       <Center>
-        <Particle count={600} />
+        <Particle count={10000} />
       </Center>
     </>
   );
@@ -27,18 +31,18 @@ const Scence = () => {
 
 const Particle = ({ count }) => {
   const bufferRef = useRef();
-  const position = new Float32Array(count * 3);
+  const pointRef = useRef();
+  const clock = new THREE.Clock();
 
+  const position = new Float32Array(count * 3);
+  const colors = new Float32Array(count * 3);
   for (let i = 0; i < count * 3; i++) {
     position[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
   }
 
-  const clock = new THREE.Clock();
-  useFrame(() => {
-    if (!bufferRef) return;
-    const elaps = clock.getElapsedTime();
-    bufferRef.current.rotateY(0.001);
-    bufferRef.current.rotateX(0.001);
+  const pointMat = useTexture({
+    alphaMap: "/textures/particles/2.png",
   });
 
   useLayoutEffect(() => {
@@ -47,13 +51,42 @@ const Particle = ({ count }) => {
       "position",
       new THREE.BufferAttribute(position, 3)
     );
+    bufferRef.current.setAttribute(
+      "color",
+      new THREE.BufferAttribute(colors, 3)
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [bufferRef.current]);
+
+  useFrame(() => {
+    if (!pointRef || !bufferRef) return;
+    const elaps = clock.getElapsedTime();
+    // pointRef.current.rotation.y = elaps * 0.2;
+    for (let i = 0; i < count; i++) {
+      let i3 = i * 3;
+      const x = bufferRef.current.attributes.position.array[i3];
+      bufferRef.current.attributes.position.array[i3 + 1] = Math.sin(elaps + x);
+    }
+
+    bufferRef.current.attributes.position.needsUpdate = true;
+  });
+
   return (
     <>
-      <points>
+      <points ref={pointRef}>
         <bufferGeometry ref={bufferRef} />
-        <pointsMaterial size={0.02} sizeAttenuation={true} />
+        <pointsMaterial
+          transparent
+          size={0.1}
+          sizeAttenuation={true}
+          // color={new THREE.Color("#ff88cc")}
+          {...pointMat}
+          // alphaTest={0.01}
+          // depthTest={false}
+          depthWrite={false} // recommendation using
+          blending={THREE.AdditiveBlending} // this make effect to performance
+          vertexColors
+        />
       </points>
     </>
   );
